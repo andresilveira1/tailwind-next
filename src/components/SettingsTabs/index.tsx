3540ed2dd31ee9ad97d5fe718af12c1a5236413c
André Silveira <andre.silveira1silva@gmail.com>
2023-11-23 10:50:26 -0300

    style: Responsive tabs

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -1,54 +1,80 @@
 'use client'
 
-import * as Tabs from '@radix-ui/react-tabs'
-import { TabItem } from './TabItem'
 import { useState } from 'react'
+
+import * as Tabs from '@radix-ui/react-tabs'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
+
+import { TabItem } from './TabItem'
 
 export function SettingsTabs() {
   const [currentTab, setCurrentTab] = useState('details')
 
   return (
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
-      <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-300">
-        <TabItem
-          value="details"
-          title="My details"
-          isSelected={currentTab === 'details'}
-        />
-        <TabItem
-          value="profile"
-          title="Profile"
-          isSelected={currentTab === 'profile'}
-        />
-        <TabItem
-          value="password"
-          title="Password"
-          isSelected={currentTab === 'password'}
-        />
-        <TabItem value="team" title="Team" isSelected={currentTab === 'team'} />
-        <TabItem value="plan" title="Plan" isSelected={currentTab === 'plan'} />
-        <TabItem
-          value="billing"
-          title="Billing"
-          isSelected={currentTab === 'billing'}
-        />
-        <TabItem
-          value="email"
-          title="Email"
-          isSelected={currentTab === 'email'}
-        />
-        <TabItem
-          value="notifications"
-          title="Notifications"
-          isSelected={currentTab === 'notifications'}
-        />
-        <TabItem
-          value="integrations"
-          title="Integrations"
-          isSelected={currentTab === 'integrations'}
-        />
-        <TabItem value="api" title="API" isSelected={currentTab === 'api'} />
-      </Tabs.List>
+      <ScrollArea.Root className="w-full" type="scroll">
+        <ScrollArea.Viewport className="w-full overflow-x-scroll">
+          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-300">
+            <TabItem
+              value="details"
+              title="My details"
+              isSelected={currentTab === 'details'}
+            />
+            <TabItem
+              value="profile"
+              title="Profile"
+              isSelected={currentTab === 'profile'}
+            />
+            <TabItem
+              value="password"
+              title="Password"
+              isSelected={currentTab === 'password'}
+            />
+            <TabItem
+              value="team"
+              title="Team"
+              isSelected={currentTab === 'team'}
+            />
+            <TabItem
+              value="plan"
+              title="Plan"
+              isSelected={currentTab === 'plan'}
+            />
+            <TabItem
+              value="billing"
+              title="Billing"
+              isSelected={currentTab === 'billing'}
+            />
+            <TabItem
+              value="email"
+              title="Email"
+              isSelected={currentTab === 'email'}
+            />
+            <TabItem
+              value="notifications"
+              title="Notifications"
+              isSelected={currentTab === 'notifications'}
+            />
+            <TabItem
+              value="integrations"
+              title="Integrations"
+              isSelected={currentTab === 'integrations'}
+            />
+            <TabItem
+              value="api"
+              title="API"
+              isSelected={currentTab === 'api'}
+            />
+          </Tabs.List>
+        </ScrollArea.Viewport>
+
+        <ScrollArea.Scrollbar
+          orientation="horizontal"
+          className="flex h-0.5 translate-y-1.5 touch-none select-none flex-col bg-zinc-100"
+        >
+          <ScrollArea.Thumb className="relative flex-1 rounded-lg bg-zinc-300" />
+        </ScrollArea.Scrollbar>
+      </ScrollArea.Root>
     </Tabs.Root>
   )
 }
